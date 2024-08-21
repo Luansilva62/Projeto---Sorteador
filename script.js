5974a1982e444d936d0dc4ea5748d7f9574af640
Luansilva62 <luan.cs.680@gmail.com>
2024-08-21 18:31:33 -0300

    Mudança de alerta - commit

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ function generateNumber() {
    const min = Math.ceil(document.querySelector(".input-min").value)
    const max = Math.floor(document.querySelector(".input-max").value)
 
-   const result = Math.floor(Math.random() * (max - min + 1) + min)
+      if(min >= max) {
+         alert("O valor MINIMO tem que ser maior que o MAXIMO!")
+      } else {
+         const result = Math.floor(Math.random() * (max - min + 1) + min)
 
-   alert(result)
+         alert(result)
+      }
+      
+      
+
+   
 }
